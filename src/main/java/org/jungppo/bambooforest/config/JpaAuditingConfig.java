7999fa2ca1598faacfa5c815c9618979564b3136
jeyong <joon6093@naver.com>
2024-07-12 03:45:29 +0900

    feat : 생성/수정 시각 자동으로 주입하도록 구현

---
+++ src/main/java/org/jungppo/bambooforest/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package org.jungppo.bambooforest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
