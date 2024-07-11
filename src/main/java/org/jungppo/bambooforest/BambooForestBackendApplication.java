058b5e68976123aaed7734dfa6edaf8f5199a68c
jeyong <joon6093@naver.com>
2024-07-11 23:10:09 +0900

    init : 프로젝트 생성

---
+++ src/main/java/org/jungppo/bambooforest/BambooForestBackendApplication.java
@@ -0,0 +1,13 @@
+package org.jungppo.bambooforest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BambooForestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BambooForestBackendApplication.class, args);
+	}
+
+}
