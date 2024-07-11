184ea483bd14052ddcd949a16c0146e13c67a71c
jeyong <joon6093@naver.com>
2024-07-12 03:45:38 +0900

    feat : 사용자 엔티티 구현

---
+++ src/main/java/org/jungppo/bambooforest/entity/member/MemberBaseEntity.java
@@ -0,0 +1,48 @@
+package org.jungppo.bambooforest.entity.member;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.jungppo.bambooforest.entity.common.JpaBaseEntity;
+import org.jungppo.bambooforest.entity.type.OAuth2Type;
+import org.jungppo.bambooforest.entity.type.RoleType;
+
+@Entity
+@Getter
+@Table(name ="member")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberBaseEntity extends JpaBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OAuth2Type oAuth2;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String profileImage;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RoleType role;
+
+    @Builder
+    public MemberBaseEntity(String name, OAuth2Type oAuth2, String username, String profileImage, RoleType role) {
+        this.name = name;
+        this.oAuth2 = oAuth2;
+        this.username = username;
+        this.profileImage = profileImage;
+        this.role = role;
+    }
+}
