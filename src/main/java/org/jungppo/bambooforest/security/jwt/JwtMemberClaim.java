5a0fe5c3965ae5b0f7e1603ad25a4d0eadeb0d46
jeyong <joon6093@naver.com>
2024-07-14 03:55:18 +0900

    refactor : Enum 클래스인 OAuth2Type에서 registrationId를 관리하도록 변경

--- src/main/java/org/jungppo/bambooforest/security/jwt/JwtMemberClaim.java
+++ src/main/java/org/jungppo/bambooforest/security/jwt/JwtMemberClaim.java
@@ -1,13 +1,15 @@
 package org.jungppo.bambooforest.security.jwt;
 
+import org.jungppo.bambooforest.entity.type.OAuth2Type;
+import org.jungppo.bambooforest.entity.type.RoleType;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.jungppo.bambooforest.entity.type.RoleType;
 
 @Getter
 @AllArgsConstructor
 public class JwtMemberClaim {
 	private final Long id;
-	private final RoleType role;
-	private final String registrationId;
+	private final RoleType roleType;
+	private final OAuth2Type oAuth2Type;
 }
--- src/main/java/org/jungppo/bambooforest/security/oauth2/CustomOAuth2UserService.java
+++ src/main/java/org/jungppo/bambooforest/security/oauth2/CustomOAuth2UserService.java
@@ -1,6 +1,7 @@
 package org.jungppo.bambooforest.security.oauth2;
 
-import lombok.RequiredArgsConstructor;
+import static org.jungppo.bambooforest.entity.type.RoleType.*;
+
 import org.jungppo.bambooforest.entity.member.MemberEntity;
 import org.jungppo.bambooforest.repository.member.MemberRepository;
 import org.jungppo.bambooforest.security.oauth2.response.OAuth2Response;
@@ -11,37 +12,38 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.jungppo.bambooforest.entity.type.RoleType.ROLE_USER;
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
-    private final MemberRepository memberRepository;
+	private final MemberRepository memberRepository;
 
-    @Override
-    @Transactional
-    public OAuth2User loadUser(OAuth2UserRequest userRequest) {
-        OAuth2User oAuth2User = super.loadUser(userRequest);
-        String registrationId = userRequest.getClientRegistration().getRegistrationId();
-        OAuth2Response oAuth2Response = OAuth2ResponseFactory.getOAuth2Response(registrationId, oAuth2User.getAttributes());
-        String name = oAuth2Response.getProvider().name() + "_" + oAuth2Response.getProviderId();
-        MemberEntity memberEntity = memberRepository.findByName(name)
-                .map(existingMember -> {
-                    existingMember.updateInfo(oAuth2Response.getName(), oAuth2Response.getProfileImage());
-                    return existingMember;
-                })
-                .orElseGet(() ->
-                        memberRepository.save(
-                                MemberEntity.builder()
-                                        .name(name)
-                                        .oAuth2(oAuth2Response.getProvider())
-                                        .username(oAuth2Response.getName())
-                                        .profileImage(oAuth2Response.getProfileImage())
-                                        .role(ROLE_USER)
-                                        .build()
-                ));
+	@Override
+	@Transactional
+	public OAuth2User loadUser(OAuth2UserRequest userRequest) {
+		OAuth2User oAuth2User = super.loadUser(userRequest);
+		String registrationId = userRequest.getClientRegistration().getRegistrationId();
+		OAuth2Response oAuth2Response = OAuth2ResponseFactory.getOAuth2Response(registrationId,
+			oAuth2User.getAttributes());
+		String name = oAuth2Response.getProvider().name() + "_" + oAuth2Response.getProviderId();
+		MemberEntity memberEntity = memberRepository.findByName(name)
+			.map(existingMember -> {
+				existingMember.updateInfo(oAuth2Response.getName(), oAuth2Response.getProfileImage());
+				return existingMember;
+			})
+			.orElseGet(() ->
+				memberRepository.save(
+					MemberEntity.builder()
+						.name(name)
+						.oAuth2(oAuth2Response.getProvider())
+						.username(oAuth2Response.getName())
+						.profileImage(oAuth2Response.getProfileImage())
+						.role(ROLE_USER)
+						.build()
+				));
 
-        return new CustomOAuth2User(memberEntity.getId(), memberEntity.getRole().name(), registrationId);
-    }
+		return new CustomOAuth2User(memberEntity.getId(), memberEntity.getRole(), memberEntity.getOAuth2());
+	}
 }
