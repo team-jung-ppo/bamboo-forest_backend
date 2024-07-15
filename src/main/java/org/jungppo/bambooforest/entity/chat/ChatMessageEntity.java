403f34a46bc766a355f2a897d7b96b595f45ad0c
jeyong <joon6093@naver.com>
2024-07-16 01:07:41 +0900

    fix : ChatMessage의 Entity에서 ChatRoom 외래키 지정을 Column으로 한 오류 수정

--- src/main/java/org/jungppo/bambooforest/entity/chat/ChatMessageEntity.java
+++ src/main/java/org/jungppo/bambooforest/entity/chat/ChatMessageEntity.java
@@ -11,25 +11,25 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 
 @Entity
 @Getter
+@Table(name = "chat_message")
 public class ChatMessageEntity extends JpaBaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "chat_room_id")
-    @Column(nullable = false)
-    private ChatRoomEntity chatRoom;
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "chat_room_id", nullable = false)
+	private ChatRoomEntity chatRoom;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "member_id")
-    @Column(nullable = false)
-    private MemberEntity member;
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "member_id", nullable = false)
+	private MemberEntity member;
 
-    @Column(nullable = false)
-    private String content;
+	@Column(nullable = false)
+	private String content;
 }
