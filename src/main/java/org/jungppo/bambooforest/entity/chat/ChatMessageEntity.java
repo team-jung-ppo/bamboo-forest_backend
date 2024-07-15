39f566eafc35bfe5998c71988ea9e72541b2e27a
Kang Dong Hyeon <ehdgus6435@kumoh.ac.kr>
2024-07-15 14:30:41 +0900

    feat : 채팅 기록 저장에 필요한 entity 작성

---
+++ src/main/java/org/jungppo/bambooforest/entity/chat/ChatMessageEntity.java
@@ -0,0 +1,33 @@
+package org.jungppo.bambooforest.entity.chat;
+
+import org.jungppo.bambooforest.entity.common.JpaBaseEntity;
+import org.jungppo.bambooforest.entity.member.MemberEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class ChatMessageEntity extends JpaBaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoomEntity chatRoom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private MemberEntity member;
+
+    @Column(nullable = false)
+    private String content;
+}
---
+++ src/main/java/org/jungppo/bambooforest/entity/chat/ChatRoomEntity.java
@@ -0,0 +1,19 @@
+package org.jungppo.bambooforest.entity.chat;
+
+import org.jungppo.bambooforest.entity.common.JpaBaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class ChatRoomEntity extends JpaBaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
