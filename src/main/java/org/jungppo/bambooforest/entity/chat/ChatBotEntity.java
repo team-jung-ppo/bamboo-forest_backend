67f9870c341b476e805b87c979cb0fa026d28482
Kang Dong Hyeon <ehdgus6435@kumoh.ac.kr>
2024-07-15 19:10:21 +0900

    feat : chatbot entity 작성

---
+++ src/main/java/org/jungppo/bambooforest/entity/chat/ChatBotEntity.java
@@ -0,0 +1,35 @@
+package org.jungppo.bambooforest.entity.chat;
+
+import org.jungppo.bambooforest.entity.common.JpaBaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class ChatBotEntity extends JpaBaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String url;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private String prompt;
+}
