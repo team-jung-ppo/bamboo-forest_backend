29b03930c12f1db7da0d73b67eb08e3030a8c93d
joon6093 <118044367+joon6093@users.noreply.github.com>
2024-07-22 07:43:03 +0900

    feat : 배터리 사용 내역을 저장하는 entity 구현

---
+++ src/main/java/org/jungppo/bambooforest/battery/domain/entity/BatteryUsageEntity.java
@@ -0,0 +1,52 @@
+package org.jungppo.bambooforest.battery.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.jungppo.bambooforest.chatbot.domain.ChatBotItem;
+import org.jungppo.bambooforest.global.jpa.domain.entity.JpaBaseEntity;
+import org.jungppo.bambooforest.member.domain.entity.MemberEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "battery_usage")
+public class BatteryUsageEntity extends JpaBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "battery_usage_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private int count;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ChatBotItem chatBotItem;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id", nullable = false)
+    private MemberEntity member;
+
+    @Builder
+    public BatteryUsageEntity(final int count, @NonNull final ChatBotItem chatBotItem,
+                              @NonNull final MemberEntity member) {
+        this.count = count;
+        this.chatBotItem = chatBotItem;
+        this.member = member;
+    }
+}
