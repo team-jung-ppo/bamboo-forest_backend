3071d346c2769eefb7783ecea1c636a531497080
joon6093 <118044367+joon6093@users.noreply.github.com>
2024-07-22 18:48:03 +0900

    refactor : 챗봇 구매 동시성 테스트의 클래스명을 변경

--- src/test/java/org/jungppo/bambooforest/ChatBotPurchaseServiceConcurrencyTest.java
+++ src/test/java/org/jungppo/bambooforest/ChatBotPurchaseServiceConcurrencyTest.java
@@ -24,7 +24,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-public class ChatBotServiceConcurrencyTest {
+public class ChatBotPurchaseServiceConcurrencyTest {
 
     @Autowired
     private ChatBotPurchaseService chatBotPurchaseService;
