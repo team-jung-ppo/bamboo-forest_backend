263e5354b9a66261dfbc8c3f1ffa44f2caa6c05c
jeyong <joon6093@naver.com>
2024-07-25 19:48:11 +0900

    chore : cors 설정에서 Origin 지정을 명시적으로 지정하도록 변경

--- src/main/java/org/jungppo/bambooforest/global/config/WebConfig.java
+++ src/main/java/org/jungppo/bambooforest/global/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
