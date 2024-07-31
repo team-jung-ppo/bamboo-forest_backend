38ab00c1d679ded6f8059c8078fef5b20a797ac3
DongHyeonka <ehdgus6435@kumoh.ac.kr>
2024-08-01 00:35:22 +0900

    fix : ChatListDto에서 of 메서드를 역할이 명확한 이름으로 변경

--- src/main/java/org/jungppo/bambooforest/chat/dto/ChatListDto.java
+++ src/main/java/org/jungppo/bambooforest/chat/dto/ChatListDto.java
@@ -9,13 +9,11 @@ import org.jungppo.bambooforest.chat.domain.entity.ChatMessageEntity;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ChatListDto {
-    private String roomId;
-    private String roomName;
     private String userMessage;
     private String botMessage;
     private String chatbotName;
 
     public static ChatListDto of(ChatMessageEntity lastMessage) {
-        return new ChatListDto(lastMessage.getChatRoom().getRoomId(), lastMessage.getChatRoom().getName(), lastMessage.getUserMessage(), lastMessage.getBotMessage(), lastMessage.getChatbotName());
+        return new ChatListDto(lastMessage.getUserMessage(), lastMessage.getBotMessage(), lastMessage.getChatbotName());
     }
 }
