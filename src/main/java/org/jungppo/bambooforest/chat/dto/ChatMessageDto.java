19db71e9d293b93df6706c5bffcfd23225801cbf
Kang Dong Hyeon <ehdgus6435@kumoh.ac.kr>
2024-08-02 17:34:08 +0900

    chore : validate 추가

--- src/main/java/org/jungppo/bambooforest/chat/dto/ChatMessageDto.java
+++ src/main/java/org/jungppo/bambooforest/chat/dto/ChatMessageDto.java
@@ -1,6 +1,9 @@
 package org.jungppo.bambooforest.chat.dto;
 
 import com.fasterxml.jackson.databind.JsonNode;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,10 +11,19 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ChatMessageDto {
+    @NotNull(message = "Message type cannot be null")
     private MessageType type;
+
+    @NotBlank(message = "Room ID cannot be blank")
     private String roomId;
+
+    @NotNull(message = "Member ID cannot be null")
     private Long memberId;
+
+    @NotBlank(message = "Message cannot be blank")
     private String message;
+
+    @NotBlank(message = "ChatBot type cannot be blank")
     private String chatBotType;
 
     public enum MessageType {
--- src/main/java/org/jungppo/bambooforest/chat/dto/ChatRoomDto.java
+++ src/main/java/org/jungppo/bambooforest/chat/dto/ChatRoomDto.java
@@ -2,6 +2,8 @@ package org.jungppo.bambooforest.chat.dto;
 
 import org.jungppo.bambooforest.chat.domain.entity.ChatRoomEntity;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,7 +11,11 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ChatRoomDto {
+    @NotBlank(message = "Room ID cannot be blank")
     private String roomId;
+    
+    @NotBlank(message = "Name cannot be blank")
+    @Size(max = 100, message = "Name cannot be longer than 100 characters")
     private String name;
 
     public static ChatRoomDto from(ChatRoomEntity chatRoomEntity) {
