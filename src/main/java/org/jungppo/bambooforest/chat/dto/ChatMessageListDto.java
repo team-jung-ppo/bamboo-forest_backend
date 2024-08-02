b6f9f6e8c1e019a9022e64b317eeb8889c71766d
Kang Dong Hyeon <ehdgus6435@kumoh.ac.kr>
2024-08-02 17:25:29 +0900

    chore : dto로 보낼 때 id 값과 createdAt을 반환

--- src/main/java/org/jungppo/bambooforest/chat/dto/ChatMessageListDto.java
+++ src/main/java/org/jungppo/bambooforest/chat/dto/ChatMessageListDto.java
@@ -4,15 +4,19 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 import org.jungppo.bambooforest.chat.domain.entity.ChatMessageEntity;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ChatMessageListDto {
+    private Long id;
     private String userMessage;
     private String botMessage;
+    private LocalDateTime createdAt;
 
     public static ChatMessageListDto from(ChatMessageEntity lastMessage) {
-        return new ChatMessageListDto(lastMessage.getUserMessage(), lastMessage.getBotMessage());
+        return new ChatMessageListDto(lastMessage.getId(), lastMessage.getUserMessage(), lastMessage.getBotMessage(), lastMessage.getCreatedAt());
     }
 }
