62aea45f6e42ff8fd5fa8f5a6ac86933e46b6b36
DongHyeonka <ehdgus6435@kumoh.ac.kr>
2024-08-03 14:24:48 +0900

    chore : 방 생성 시간 dto에 추가

--- src/main/java/org/jungppo/bambooforest/chat/dto/ChatRoomDto.java
+++ src/main/java/org/jungppo/bambooforest/chat/dto/ChatRoomDto.java
@@ -2,6 +2,9 @@ package org.jungppo.bambooforest.chat.dto;
 
 import org.jungppo.bambooforest.chat.domain.entity.ChatRoomEntity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,8 +13,11 @@ import lombok.Getter;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ChatRoomDto {
     private String roomId;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
     
     public static ChatRoomDto from(final ChatRoomEntity chatRoomEntity) {
-        return new ChatRoomDto(chatRoomEntity.getRoomId());
+        return new ChatRoomDto(chatRoomEntity.getRoomId(), chatRoomEntity.getCreatedAt());
     }
 }
