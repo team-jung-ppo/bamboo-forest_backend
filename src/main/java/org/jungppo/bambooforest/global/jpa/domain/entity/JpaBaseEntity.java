de29683e819f83c28fc60a2499cdbc3e5100fc42
jeyong <joon6093@naver.com>
2024-08-05 16:18:00 +0900

    refactor : baseEntity에 기본 생성자를 명시적으로 지정

--- src/main/java/org/jungppo/bambooforest/global/jpa/domain/entity/JpaBaseEntity.java
+++ src/main/java/org/jungppo/bambooforest/global/jpa/domain/entity/JpaBaseEntity.java
@@ -5,12 +5,14 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
+@NoArgsConstructor
 @Getter
 public abstract class JpaBaseEntity {
     @CreatedDate
