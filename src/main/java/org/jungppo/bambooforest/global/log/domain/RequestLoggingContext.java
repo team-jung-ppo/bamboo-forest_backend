5f6ed5f44eb7e3e41fa4361e4ae808f1eba22d65
jeyong <joon6093@naver.com>
2024-08-27 14:21:25 +0900

    fix : NplusOneDetector와 동일한 빈 이름을 사용하는 오류 해결

--- src/main/java/org/jungppo/bambooforest/global/log/domain/RequestLoggingContext.java
+++ src/main/java/org/jungppo/bambooforest/global/log/domain/RequestLoggingContext.java
@@ -4,7 +4,7 @@ import org.jungppo.bambooforest.global.log.dto.RequestLogDto;
 import org.springframework.stereotype.Component;
 
 @Component
-public class LoggingContext {
+public class RequestLoggingContext {
     private final ThreadLocal<RequestLogDto> currentLoggingForm = ThreadLocal.withInitial(RequestLogDto::new);
 
     public RequestLogDto getCurrentLoggingForm() {
